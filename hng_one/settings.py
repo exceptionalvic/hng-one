07f9b09d07082d6e508f8d49d1e361a0c5e3f844
Victor Igbokwe <igbokwev@gmail.com>
2024-07-04 15:29:01 +0100

    update gitignore and settings.py

--- hng_one/settings.py
+++ hng_one/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['collie-modest-repeatedly.ngrok-free.app','localhost']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',')
 
 
 # Application definition
